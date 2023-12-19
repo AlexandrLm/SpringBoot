a9d691fd1c78f7a0989fdff2ad38b22350e916e6
Александр Ламожапов <vikal1709@gmail.com>
2023-12-20 00:17:19 +0800

    Initial commit

---
+++ src/main/java/com/example/moviespringboot/MoviesRepository.java
@@ -0,0 +1,11 @@
+package com.example.moviespringboot;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MoviesRepository extends JpaRepository<Movies, Long> {
+    public List<Movies> findByTitleIgnoreCase (String title);
+}
